80ee732a6d813adbbe59ace7a10676a4b4f2a7ff
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-04-03 00:08:27 +0530

    ADDED LOGS

--- auth_service/src/main/java/com/ebms/controller/AuthController.java
+++ auth_service/src/main/java/com/ebms/controller/AuthController.java
@@ -28,6 +28,8 @@ public class AuthController {
         if(consumerRepository.findByEmail(request.getEmail()).isPresent()) {
             return ResponseEntity.status(409).body("Email already in use");
         }
+        System.out.println("PWD: " + request.getPassword());
+        System.out.println("PWD: " + passwordEncoder.encode(request.getPassword()));
         final var consumer = Consumer.builder()
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
