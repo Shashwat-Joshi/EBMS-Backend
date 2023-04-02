8eb5e035cf827543871014a5da2bb0526574a7cd
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-04-02 15:11:45 +0530

    allowed all origins for CORS issue

--- auth_service/src/main/java/com/ebms/controller/AuthController.java
+++ auth_service/src/main/java/com/ebms/controller/AuthController.java
@@ -9,10 +9,7 @@ import com.ebms.schema.EBMSRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.Optional;
